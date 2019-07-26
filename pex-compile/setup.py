f282f044fb656dc5c51b42420c456ea75739a6d0
Alexander Korzun <korzun.sas@mail.ru>
2019-07-27 01:48:33 +0300

    Update setup.py info

--- pex-compile/setup.py
+++ pex-compile/setup.py
@@ -9,9 +9,18 @@ setup(
     description         = 'A program to compile Python files to PEX format for Pyke',
     author              = 'Alexander Korzun',
     author_email        = 'korzun.sas@mail.ru',
-    license             = 'MIT',
     packages            = ['pex_compile'],
     entry_points        = {
         'console_scripts': ['pex-compile=pex_compile.__main__:main'],
     },
+    classifiers         = [
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Compilers',
+    ]
 )
