4ea221d693bc86667e10bb101504bd8513a95eea
Alexander Korzun <korzun.sas@mail.ru>
2019-08-08 15:22:43 +0300

    Add code pretty-printing

--- pex-compile/pex_compile/__main__.py
+++ pex-compile/pex_compile/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from pex_compile import ast_to_pykebc
+from pex_compile import pykebc
 #from pex_compile import build_pex
 
 import ast
@@ -15,13 +16,40 @@ def parse_args():
     return ap.parse_args()
 
 
+def indent(string, indent_string=' '*4):
+    lines = string.split('\n')
+    return '\n'.join([
+        indent_string + line
+        for line in lines
+    ])
+
+
+def pretty(code):
+    buf = []
+    for command, argument in code.instructions:
+        if argument is None:
+            buf.append(command + '\n')
+        else:
+            buf.append(command + ' ' + str(argument) + '\n')
+    buf.append('\n')
+    for i, value in enumerate(code.constants):
+        buf.append(
+            'const {} = {}\n'.format(
+                i,
+                pretty(value) if isinstance(value, pykebc.LinkedCode) else repr(value),
+            )
+        )
+    return '{\n' + indent(''.join(buf)).rstrip() + '\n' + '}'
+
+
 def main():
     options = parse_args()
     with open(options.source, 'r') as f:
         code = f.read()
     tree = ast.parse(code)
     pyke_bytecode = ast_to_pykebc.translate(tree)
-    print(pyke_bytecode.asm())
+    linked_code = pyke_bytecode.link()
+    print(pretty(linked_code))
     #pex_file = build_pex.build(pyke_bytecode)
     #with open(options.output, 'wb') as f:
     #    f.write(pex_file.to_bytes())
